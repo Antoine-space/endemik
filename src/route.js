6d909e92ff1d07044db9c56bf15f6b6a57c11a12
valensto <v.e.brochard@gmail.com>
2020-12-11 14:32:22 +0100

    add console log route.js

--- src/route.js
+++ src/route.js
@@ -1,6 +1,8 @@
 const pagesHandlers = require("./handlers/pages")
 const getCurrent = require("./handlers/weather")
 
+console.log("test")
+
 const initRoutes = (app) => {
   app.get("/", pagesHandlers.home);
   app.get("/contact", pagesHandlers.contact);
